a8e3339768ddda5f1c5eaddc37babb4b46e7bfbe
Tyler Kieluk <tyler.kieluk@usask.ca>
2022-02-12 22:06:37 -0600

    Restarting fpga

--- monitor_fpga/monitor_fpga.sv
+++ monitor_fpga/monitor_fpga.sv
@@ -1,8 +1,17 @@
 module monitor_fpga(
-    input clk, reset,
-    output reset_flag
+    input  wire clk, 
+    input  wire reset,
+    output wire reset_flag,
+
+    // UART
+    input  wire uart_rxd, // receiver
+    output wire uart_txd, // transmitter
+    input  wire uart_cts, // clear to send
+    output wire uart_rts  // request to send
 );
 
 assign reset_flag = ~reset;
+
+
 
 endmodule
