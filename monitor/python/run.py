cb93460546cce535a6162043b619043412f4de1f
Tyler Kieluk <tyler.kieluk@usask.ca>
2022-02-16 22:09:53 -0600

    Skeleton for run.py main entrance point

---
+++ monitor/python/run.py
@@ -0,0 +1,30 @@
+# Imports ######################################################################
+import subprocess
+import sys
+
+# Globals ######################################################################
+
+
+# Library ######################################################################
+def setup():
+    """
+    Install packages needed.
+
+    pip3 install pipreqs
+    pipreqs .
+
+    pip3 install -r requirements.txt
+
+    TODO: IMPLEMENT ME
+    """
+    pass
+
+# Main #########################################################################
+def main():
+    """
+    Main entrance point to the monitor/controller.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
